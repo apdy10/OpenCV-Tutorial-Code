cb79bd20b20265fedc8d27f6bc4238956891b661
Arindam Priyadarshi <51259879+apdy10@users.noreply.github.com>
2021-02-28 13:55:54 +0530

    Update Other method to convert RGB to Gray Scale Image without using cvtColor.py

--- Other method to convert RGB to Gray Scale Image without using cvtColor.py
+++ Other method to convert RGB to Gray Scale Image without using cvtColor.py
@@ -1,5 +1,6 @@
+# By using cvtColor function we have many other options too
 import cv2
-img= cv2.imread("Bride.jpg",0)
+img= cv2.imread("Bride.jpg",0) 
 cv2.imshow("gray scale image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
